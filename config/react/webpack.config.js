bc4c9c771dba115d054088a62f1666e928ae66eb
zzuu666 <zzuu666@outlook.com>
2019-12-23 20:03:19 +0800

    build(webpack): add some build configrations to improve performance

--- config/react/webpack.config.js
+++ config/react/webpack.config.js
@@ -1,8 +1,20 @@
 const webpack = require('webpack')
 const fs = require('fs')
+const isWsl = require('is-wsl')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const safePostCssParser = require('postcss-safe-parser')
+const TerserPlugin = require('terser-webpack-plugin')
 
+// Make sure '.env' file is requird before 'paths'
+const getClientEnvironment = require('./env')
 const paths = require('./paths')
+const InterpolateHtmlPlugin = require('../../extras/react-dev-utils/InterpolateHtmlPlugin')
+const WatchMissingNodeModulesPlugin = require('../../extras/react-dev-utils/WatchMissingNodeModulesPlugin')
+
+const appPackageJson = require(paths.appPackageJson)
 
 // Source maps are resource heavy and can cause out of memory issue for large source files.
 const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false'
@@ -10,6 +22,12 @@ const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false'
 module.exports = webpackEnv => {
     const isEnvDevelopment = webpackEnv === 'development'
     const isEnvProduction = webpackEnv === 'production'
+
+    // Variable used for enabling profiling in Production
+    // passed into alias object. Uses a flag if passed into the build command
+    const isEnvProductionProfile =
+        isEnvProduction && process.argv.includes('--profile')
+
     const useTypeScript = fs.existsSync(paths.appTsConfig)
 
     // Webpack uses `publicPath` to determine where the app is being served from.
@@ -18,6 +36,19 @@ module.exports = webpackEnv => {
     const publicPath = isEnvProduction
         ? paths.servedPath
         : isEnvDevelopment && '/'
+    // Some apps do not use client-side routing with pushState.
+    // For these, "homepage" can be set to "." to enable relative asset paths.
+    const shouldUseRelativeAssetPaths = publicPath === './'
+
+    // `publicUrl` is just like `publicPath`, but we will provide it to our app
+    // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
+    // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
+    const publicUrl = isEnvProduction
+        ? publicPath.slice(0, -1)
+        : isEnvDevelopment && ''
+
+    // Get environment variables to inject into our app.
+    const env = getClientEnvironment(publicUrl)
 
     return {
         mode: isEnvProduction
@@ -28,7 +59,111 @@ module.exports = webpackEnv => {
         entry: [paths.appIndexJs],
         output: {
             // The build folder.
-            path: isEnvProduction ? paths.appBuild : undefined
+            path: isEnvProduction ? paths.appBuild : undefined,
+            // Add /* filename */ comments to generated require()s in the output.
+            pathinfo: isEnvDevelopment,
+            // There will be one main bundle, and one file per asynchronous chunk.
+            // In development, it does not produce real files.
+            filename: isEnvProduction
+                ? 'static/js/[name].[contenthash:8].js'
+                : isEnvDevelopment && 'static/js/bundle.js',
+            // TODO: remove this when upgrading to webpack 5
+            futureEmitAssets: true,
+            // There are also additional JS chunk files if you use code splitting.
+            chunkFilename: isEnvProduction
+                ? 'static/js/[name].[contenthash:8].chunk.js'
+                : isEnvDevelopment && 'static/js/[name].chunk.js',
+            // We inferred the "public path" (such as / or /my-project) from homepage.
+            // We use "/" in development.
+            publicPath: publicPath,
+            // Prevents conflicts when multiple Webpack runtimes (from different apps)
+            // are used on the same page.
+            jsonpFunction: `webpackJsonp${appPackageJson.name}`,
+            // this defaults to 'window', but by setting it to 'this' then
+            // module chunks which are built will work in web workers as well.
+            globalObject: 'this'
+        },
+        optimization: {
+            minimize: isEnvProduction,
+            minimizer: [
+                // This is only used in production mode
+                new TerserPlugin({
+                    terserOptions: {
+                        parse: {
+                            // We want terser to parse ecma 8 code. However, we don't want it
+                            // to apply any minification steps that turns valid ecma 5 code
+                            // into invalid ecma 5 code. This is why the 'compress' and 'output'
+                            // sections only apply transformations that are ecma 5 safe
+                            // https://github.com/facebook/create-react-app/pull/4234
+                            ecma: 8
+                        },
+                        compress: {
+                            ecma: 5,
+                            warnings: false,
+                            // Disabled because of an issue with Uglify breaking seemingly valid code:
+                            // https://github.com/facebook/create-react-app/issues/2376
+                            // Pending further investigation:
+                            // https://github.com/mishoo/UglifyJS2/issues/2011
+                            comparisons: false,
+                            // Disabled because of an issue with Terser breaking valid code:
+                            // https://github.com/facebook/create-react-app/issues/5250
+                            // Pending further investigation:
+                            // https://github.com/terser-js/terser/issues/120
+                            inline: 2
+                        },
+                        mangle: {
+                            safari10: true
+                        },
+                        // Added for profiling in devtools
+                        keep_classnames: isEnvProductionProfile,
+                        keep_fnames: isEnvProductionProfile,
+                        output: {
+                            ecma: 5,
+                            comments: false,
+                            // Turned on because emoji and regex is not minified properly using default
+                            // https://github.com/facebook/create-react-app/issues/2488
+                            ascii_only: true
+                        }
+                    },
+                    // Use multi-process parallel running to improve the build speed
+                    // Default number of concurrent runs: os.cpus().length - 1
+                    // Disabled on WSL (Windows Subsystem for Linux) due to an issue with Terser
+                    // https://github.com/webpack-contrib/terser-webpack-plugin/issues/21
+                    parallel: !isWsl,
+                    // Enable file caching
+                    cache: true,
+                    sourceMap: shouldUseSourceMap
+                }),
+                // This is only used in production mode
+                new OptimizeCSSAssetsPlugin({
+                    cssProcessorOptions: {
+                        parser: safePostCssParser,
+                        map: shouldUseSourceMap
+                            ? {
+                                  // `inline: false` forces the sourcemap to be output into a
+                                  // separate file
+                                  inline: false,
+                                  // `annotation: true` appends the sourceMappingURL to the end of
+                                  // the css file, helping the browser find the sourcemap
+                                  annotation: true
+                              }
+                            : false
+                    }
+                })
+            ],
+            // Automatically split vendor and commons
+            // https://twitter.com/wSokra/status/969633336732905474
+            // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
+            splitChunks: {
+                chunks: 'all',
+                name: false
+            },
+            // Keep the runtime chunk separated to enable long term caching
+            // https://twitter.com/wSokra/status/969679223278505985
+            // https://github.com/facebook/create-react-app/issues/5358
+            runtimeChunk: {
+                name: entrypoint => `runtime-${entrypoint.name}`
+            }
         },
         resolve: {
             // This allows you to set a fallback for where Webpack should look for modules.
@@ -124,7 +259,42 @@ module.exports = webpackEnv => {
                           }
                         : undefined
                 )
-            )
-        ]
+            ),
+            // Makes some environment variables available in index.html.
+            // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
+            // <link rel="icon" href="%PUBLIC_URL%/favicon.ico">
+            // In production, it will be an empty string unless you specify "homepage"
+            // in `package.json`, in which case it will be the pathname of that URL.
+            // In development, this will be an empty string.
+            new InterpolateHtmlPlugin(HtmlWebpackPlugin, env.raw),
+            // Makes some environment variables available to the JS code, for example:
+            // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
+            // It is absolutely essential that NODE_ENV is set to production
+            // during a production build.
+            // Otherwise React will be compiled in the very slow development mode.
+            new webpack.DefinePlugin(env.stringified),
+            // This is necessary to emit hot updates (currently CSS only):
+            isEnvDevelopment && new webpack.HotModuleReplacementPlugin(),
+            // Watcher doesn't work well if you mistype casing in a path so we use
+            // a plugin that prints an error when you attempt to do this.
+            // See https://github.com/facebook/create-react-app/issues/240
+            isEnvDevelopment && new CaseSensitivePathsPlugin(),
+            // If you require a missing module and then `npm install` it, you still have
+            // to restart the development server for Webpack to discover it. This plugin
+            // makes the discovery automatic so you don't have to restart.
+            // See https://github.com/facebook/create-react-app/issues/186
+            isEnvDevelopment &&
+                new WatchMissingNodeModulesPlugin(paths.appNodeModules),
+            isEnvProduction &&
+                new MiniCssExtractPlugin({
+                    // Options similar to the same options in webpackOptions.output
+                    // both options are optional
+                    filename: 'static/css/[name].[contenthash:8].css',
+                    chunkFilename: 'static/css/[name].[contenthash:8].chunk.css'
+                })
+        ].filter(Boolean),
+        // Turn off performance processing because we utilize
+        // our own hints via the FileSizeReporter
+        performance: false
     }
 }
