02d63b8c32d270d5a87d64f671e4b328a65a345f
zzuu666 <zzuu666@outlook.com>
2019-12-26 14:52:37 +0800

    chore(icons): add icons build configrations

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,10 @@ module.exports = {
         {
             test: './applications/*',
             presets: ['react-app']
+        },
+        {
+            test: './packages/*',
+            presets: ['@babel/env', '@babel/react', '@babel/typescript']
         }
     ]
 }
