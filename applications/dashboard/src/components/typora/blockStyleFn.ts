022021892e9334a1c885f803c826d02013a84309
zzuu666 <zzuu666@outlook.com>
2020-01-20 15:32:17 +0800

    feat(dashboard): add blockquote style

--- applications/dashboard/src/components/typora/blockStyleFn.ts
+++ applications/dashboard/src/components/typora/blockStyleFn.ts
@@ -4,11 +4,29 @@ import { BlockType } from './blockTypes'
 
 export const useBlockStyles = makeStyles({
     blockquote: {
-        color: 'red'
+        display: 'flex',
+        color: '',
+        fontSize: '20px',
+        lineHeight: 3,
+
+        '&:before': {
+            content: '"\'\'"',
+            display: '',
+            marginRight: 8
+        },
+        '&:after': {
+            content: '"\'\'"',
+            display: '',
+            marginLeft: 8
+        }
+    },
+    unstyled: {
+        fontSize: '16px',
+        lineHeight: 2.5
     }
 })
 
-export type BlockStyleClasses = Record<'blockquote', string>
+export type BlockStyleClasses = Record<'blockquote' | 'unstyled', string>
 
 export const createBlockStyleFn = (classes: BlockStyleClasses) => (
     contentBlock: ContentBlock
@@ -18,8 +36,10 @@ export const createBlockStyleFn = (classes: BlockStyleClasses) => (
     switch (type) {
         case 'blockquote':
             return classes.blockquote
-        case 'title':
+        case BlockType.title:
             return ''
+        case BlockType.unstyled:
+            return classes.unstyled
         default:
             return ''
     }
