bf7cfa435ef20519871f267483476e2e5dcb752e
zzuu666 <zzuu666@outlook.com>
2020-03-22 17:58:54 +0800

    feat(bamboo): add decleartion files and update header logo

--- applications/bamboo/components/header/index.tsx
+++ applications/bamboo/components/header/index.tsx
@@ -1,12 +1,21 @@
 import React, { FC } from 'react'
 import { createUseStyles } from 'react-jss'
+import { Image } from '../image'
+import { Grid } from '../grid'
 
 const useStyles = createUseStyles(theme => ({
     root: {
         display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center',
-        height: '64px'
+        height: '80px'
+    },
+    right: {
+        display: 'flex',
+        alignItems: 'center',
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#F0A1A8'
     }
 }))
 
@@ -14,8 +23,11 @@ export const Header: FC = props => {
     const classes = useStyles()
     return (
         <div className={classes.root}>
-            <div>左侧布局</div>
-            <div>右侧</div>
+            <div className={classes.right}>
+                <img src="/logo.svg" alt="site icon" width="32" />
+                大白话家
+            </div>
+            <div>其他</div>
         </div>
     )
 }
---
+++ applications/bamboo/interfaces/api.d.ts
@@ -0,0 +1,11 @@
+declare namespace bamboo.api {
+    export interface Article {
+        id: number
+        desc: string
+        title: string
+        image: string
+        content: string
+        createdAt: string
+        updatedAt: string
+    }
+}
