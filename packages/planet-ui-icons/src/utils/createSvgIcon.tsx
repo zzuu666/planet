ceebbfdebfa3dd33238d27ce62e37dc286a1b3fc
zzuu666 <zzuu666@outlook.com>
2020-05-18 21:11:48 +0800

    feat(bamboo): enhance createSvgIcon types

--- packages/planet-ui-icons/src/utils/createSvgIcon.tsx
+++ packages/planet-ui-icons/src/utils/createSvgIcon.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from 'react'
 
 export default function createSvgIcon(path, displayName) {
-  const Component = React.memo(
-    React.forwardRef((props, ref) => (
-      <svg ref={ref} {...props}>
-        {path}
-      </svg>
-    )),
-  );
+    const Component = React.memo(
+        React.forwardRef<SVGSVGElement, React.SVGProps<SVGSVGElement>>(
+            (props, ref) => (
+                <svg ref={ref} viewBox="0 0 24 24" {...props}>
+                    {path}
+                </svg>
+            )
+        )
+    )
 
-  if (process.env.NODE_ENV !== 'production') {
-    Component.displayName = `${displayName}Icon`;
-  }
+    if (process.env.NODE_ENV !== 'production') {
+        Component.displayName = `${displayName}Icon`
+    }
 
-  return Component;
+    return Component
 }
